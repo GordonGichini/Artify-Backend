9f84b2bdbbf1bcefd2ee26295d1601d824380998
Gordon <gordongichini1@gmail.com>
2024-01-29 20:49:58 +0300

    initial commit

---
+++ CategoryService/Models/Dtos/CategoryDto.cs
@@ -0,0 +1,7 @@
+namespace Categoryservice.Models.Dtos
+{
+    public class CategoryDto
+    {
+        public string Name { get; set; } = string.Empty;
+    }
+}
---
+++ CategoryService/Program.cs
@@ -0,0 +1,43 @@
+using Categoryservice.Data;
+using Categoryservice.Extension;
+using Microsoft.EntityFrameworkCore;
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.AddAuth();
+builder.AddSwaggenGenExtension();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("myconnection"));
+});
+
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseMigrations();
+app.UseAuthentication();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
+
